e31aa6eb6fa77666f3c95681532539b21ffe4e97
simminni <simminni@gmail.com>
2023-01-11 06:12:34 -0500

    added sample payload structure

--- model/payload.go
+++ model/payload.go
@@ -33,3 +33,46 @@ type Maintainer struct {
 	Name  string `yaml:"name"`
 	Email string `yaml:"email"`
 }
+
+/* Sample app_metadata data structure to persist inmemory
+var app_metadatas = []model.App_metadata{
+	{
+		Title:   "Valid App 1",
+		Version: "0.0.1",
+		Maintainers: []model.Maintainer{
+			{
+				Name:  "firstmaintainer app1",
+				Email: "firstmaintainer@hotmail.com",
+			},
+			{
+				Name:  "secondmaintainer app1",
+				Email: "secondmaintainer@gmail.com",
+			},
+		},
+		Company: "Random Inc.",
+		Website: "https://website.com",
+		Source:  "https://github.com/random/repo",
+		License: "Apache-2.0",
+		Description: `
+		 		### Interesting Title
+		 		Some application content, and description`,
+	},
+	{
+		Title:   "Valid App 2",
+		Version: "1.0.1",
+		Maintainers: []model.Maintainer{
+			{
+				Name:  "AppTwo Maintainer",
+				Email: "apptwo@hotmail.com",
+			},
+		},
+		Company: "Upbound Inc.",
+		Website: "https://upbound.io",
+		Source:  "https://github.com/upbound/repo",
+		License: "Apache-2.0",
+		Description: `
+		 		### Why app 2 is the best
+				 Because it simply is...`,
+	},
+}
+*/
